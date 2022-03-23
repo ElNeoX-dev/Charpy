2a86f3353e6f21953f7c85d9bca1c0e0d6a6a001
BaptisteTheBatman <99688410+BaptisteTheBatman@users.noreply.github.com>
2022-03-23 14:49:12 +0100

    mod

---
+++ Materiau.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+
+public class Materiau {
+    // ATTRIBUTS 
+    String Nom;
+    double Resilience;
+    Color Couleur;
+
+    
+    public Materiau (String unNom, double uneResilience, Color uneCouleur) {
+        Nom=unNom;
+        Resilience=uneResilience;
+        Couleur=uneCouleur;
+    }
+
+   
+
+}
