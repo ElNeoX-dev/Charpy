541f41d605b65727148c3390343b0cfcb9014850
ElNeoX-dev <61790793+ElNeoX-dev@users.noreply.github.com>
2022-04-13 15:42:33 +0200

    papapa

--- Pendule.java
+++ Pendule.java
@@ -19,50 +19,54 @@ public class Pendule {
     int finY;
     double energieInitiale;
     double energieCinetique;
-    Fenetre fen;
+    Eprouvette ep;
     int incrémentationEuler = 0;
 
     
-    public Pendule (int uneMasse, int uneLongueur, double unAngleInitial, double uneVitesseInitiale, double unFrottement, Fenetre fen) {
+    public Pendule (int uneMasse, int uneLongueur, double unAngleInitial, double uneVitesseInitiale, double unFrottement, Eprouvette ep) {
         this.masse = uneMasse;
         this.longueur = uneLongueur;
         this.longueurReelle = longueur / 100.0;
         theta.add(unAngleInitial);
         omega.add(uneVitesseInitiale);
-        this.fen = fen;
+        this.ep = ep;
         this.frottements = unFrottement;
     }
 
     public void dessine (Graphics g) {
         g.setColor(Color.blue);
-        theta.add(theta.getLast() + 0.015 * omega.getLast());
-        omega.add(omega.getLast() + 0.015 * (-longueurReelle*Math.sin(theta.get(theta.size() - 2) / 9.81 + frottements * omega.getLast())));
         centreX = 600;
         centreY = 100;
         finX = (int) (centreX + longueur * Math.sin(theta.getLast()));
         finY = (int) (centreY + longueur * Math.cos(theta.getLast()));
         int r = largeur/2;
         tabX[0] = centreX - longueur * (int)Math.cos(theta.getLast());
+        theta.add(theta.getLast() + 0.015 * omega.getLast());
+        omega.add(omega.getLast() + 0.015 * (-longueurReelle*Math.sin(theta.get(theta.size() - 2) / 9.81 + frottements * omega.getLast())));
         g.setColor(Color.blue);
         g.drawLine(centreX, centreY, finX, finY);
         g.setColor(Color.red);
         g.fillOval(finX - 50, finY - 50, 100, 100);
         //tabX[0] = centreX - r*Math.cos(angle);
-
-
-        
+        testCollision();
+ 
      }
 
-     public boolean testCollision(Eprouvette e) {
-        if(e.estVivant) {
-            if(theta.getLast() == 0) {
-                if(energieCinetique < e.unMateriau.Resilience) {
+     public void majEnergie() {
+         this.energieCinetique = 1.0/2.0 * this.masse * Math.pow(this.longueurReelle * omega.getLast(), 2);
+     }
 
-                }
-                return(true);
-            }
+     public void testCollision() {
+        if(ep.estVivant && theta.getLast() < 0.01 && theta.getLast() > - 0.01 && energieCinetique < ep.unMateriau.Resilience) {
+            theta.clear();
+            omega.clear();
+            energieCinetique -= ep.unMateriau.Resilience;
+            theta.add(0.0);
+            omega.add(Math.sqrt(2 * energieCinetique / this.masse) / this.longueurReelle);
+            
         }
-        return (false);
+
+
      }
 
      public String toString() {
