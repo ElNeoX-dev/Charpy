6dabf6f9168b2d7041f66d101a9d69c25ddd5974
ElNeoX-dev <61790793+ElNeoX-dev@users.noreply.github.com>
2022-04-19 11:34:50 +0200

    Grosse update

--- Fenetre.java
+++ Fenetre.java
@@ -47,17 +47,22 @@ public class Fenetre extends JFrame implements ActionListener {
         setLocation(0, 0);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        chrono = new Timer(1, this);
-		BD=new BaseDonneeMateriaux();
 
+        chrono = new Timer(1, this);
+	
+    
+        BD=new BaseDonneeMateriaux();
         String[] listeMat = new String[BD.maListeMateriau.size()];
         for (int i = 0; i < listeMat.length; i++) {
             listeMat[i] = BD.maListeMateriau.get(i).Nom;
         }
+
         choixMat = new JComboBox<String>(listeMat);
         choixMat.setBounds(20, 630, 120, 50);
+
+
         this.ep = new Eprouvette(BD.maListeMateriau.get(0), 100, 500);
-        this.p = new Pendule(1, 500, 0, 0, 0, ep);
+        this.p = new Pendule(1, 500, 0, 0, 0, ep, this);
 
         // conteneurs
         monConteneur1 = new JPanel();
@@ -78,6 +83,7 @@ public class Fenetre extends JFrame implements ActionListener {
         monConteneurMain.add(monConteneur1);
         monConteneurMain.add(monConteneur2);
         this.add(monConteneurMain);
+
 
         // boutons
         lancement = new JButton("Lancer");
@@ -114,7 +120,7 @@ public class Fenetre extends JFrame implements ActionListener {
         tailleTige.setBounds(160, 600, 120, 30);
         monConteneur1.add(tailleTige);
 
-        TxtTailleTige = new JTextField(""); // taille tige
+        TxtTailleTige = new JTextField(5); // taille tige
         TxtTailleTige.setBounds(160, 640, 120, 60);
         TxtTailleTige.setBackground(Color.white);
         monConteneur1.add(TxtTailleTige);
@@ -124,7 +130,7 @@ public class Fenetre extends JFrame implements ActionListener {
         angleInitial.setBounds(20, 360, 120, 30);
         monConteneur1.add(angleInitial);
 
-        TxtAngleInitial = new JTextField(""); // hauteur départ
+        TxtAngleInitial = new JTextField(0); // hauteur départ
         TxtAngleInitial.setBounds(20, 400, 120, 60);
         TxtAngleInitial.setBackground(Color.white);
         monConteneur1.add(TxtAngleInitial);
@@ -134,7 +140,7 @@ public class Fenetre extends JFrame implements ActionListener {
         masseMarteau.setBounds(160, 360, 120, 30);
         monConteneur1.add(masseMarteau);
 
-        TxtMasseMarteau = new JTextField(""); // masse marteau
+        TxtMasseMarteau = new JTextField(1); // masse marteau
         TxtMasseMarteau.setBounds(160, 400, 120, 60);
         TxtMasseMarteau.setBackground(Color.white);
         monConteneur1.add(TxtMasseMarteau);
@@ -144,7 +150,7 @@ public class Fenetre extends JFrame implements ActionListener {
         vitesseInitiale.setBounds(20, 480, 120, 30);
         monConteneur1.add(vitesseInitiale);
 
-        TxtVinit = new JTextField(""); // Vinit
+        TxtVinit = new JTextField(0); // Vinit
         TxtVinit.setBounds(20, 520, 120, 60);
         TxtVinit.setBackground(Color.white);
         monConteneur1.add(TxtVinit);
@@ -154,7 +160,7 @@ public class Fenetre extends JFrame implements ActionListener {
         epaisseurEprouvette.setBounds(160, 480, 120, 30);
         monConteneur1.add(epaisseurEprouvette);
 
-        TxtEpaisseurEprouvette = new JTextField(""); // épaisseur éprouvette
+        TxtEpaisseurEprouvette = new JTextField(0); // épaisseur éprouvette
         TxtEpaisseurEprouvette.setBounds(160, 520, 120, 60);
         TxtEpaisseurEprouvette.setBackground(Color.white);
         monConteneur1.add(TxtEpaisseurEprouvette);
@@ -170,21 +176,23 @@ public class Fenetre extends JFrame implements ActionListener {
         CoefFrottements.setPaintTicks(true);
         CoefFrottements.setPaintLabels(true);
 
+
         setVisible(true);
     }
 
-    public Timer getChrono() {
-        return(chrono);
-    }
-
-    public void setTemps(double t) {
-        tempsMs = t;
+    public void setChrono(boolean etat) {
+        if(etat == false) {
+            chrono.stop();
+        } else {
+            chrono.start();
+        }
     }
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == chrono && lancement.getBackground() == Color.green) {
             repaint();
         }
+
 
         if (e.getSource() == lancement) {
             if (lancement.getBackground() == Color.red) {
@@ -196,16 +204,18 @@ public class Fenetre extends JFrame implements ActionListener {
             }
         }
 
-        if (e.getSource() == majPendule) {
-            chrono.stop();
-            double frottements = CoefFrottements.getValue() / 1000.0;
-            this.ep = new Eprouvette(BD.maListeMateriau.get(choixMat.getSelectedIndex()), 2,
-            Integer.parseInt(TxtTailleTige.getText()));
 
-            this.ep.hauteur = Integer.parseInt(TxtTailleTige.getText());
+        if (e.getSource() == majPendule) {
+
+            chrono.stop();
+
+            double frottements = CoefFrottements.getValue() / 1000.0;
+
+            this.ep = new Eprouvette(BD.maListeMateriau.get(choixMat.getSelectedIndex()), 2,
+            Integer.parseInt(TxtTailleTige.getText())); 
             
-            this.p = new Pendule(Integer.parseInt(TxtMasseMarteau.getText()), Integer.parseInt(TxtTailleTige.getText()),
-                    Double.parseDouble(TxtAngleInitial.getText()), Double.parseDouble(TxtVinit.getText()), frottements, ep);
+            p.resetPendule(Integer.parseInt(TxtMasseMarteau.getText()), Integer.parseInt(TxtTailleTige.getText()),
+            Double.parseDouble(TxtAngleInitial.getText()), Double.parseDouble(TxtVinit.getText()), frottements, ep);
 
 
             monConteneur2.maj(p, ep);
@@ -213,6 +223,8 @@ public class Fenetre extends JFrame implements ActionListener {
             repaint();
         }
     }
+
+
     /*
      * public void degreRadian (){
      * this.angleInitial = angleInitial*Math.PI/180;
--- Pendule.java
+++ Pendule.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.*;
+import javax.swing.*;
 
 public class Pendule {
 
@@ -21,9 +22,23 @@ public class Pendule {
     double energieCinetique;
     Eprouvette ep;
     int incrémentationEuler = 0;
+    Fenetre f;
 
     
-    public Pendule (int uneMasse, int uneLongueur, double unAngleInitial, double uneVitesseInitiale, double unFrottement, Eprouvette ep) {
+    public Pendule (int uneMasse, int uneLongueur, double unAngleInitial, double uneVitesseInitiale, double unFrottement, Eprouvette ep, Fenetre f) {
+        this.masse = uneMasse;
+        this.longueur = uneLongueur;
+        this.longueurReelle = longueur / 100.0;
+        theta.add(unAngleInitial);
+        omega.add(uneVitesseInitiale);
+        this.ep = ep;
+        this.frottements = unFrottement;
+        this.f = f;
+    }
+
+    public void resetPendule(int uneMasse, int uneLongueur, double unAngleInitial, double uneVitesseInitiale, double unFrottement, Eprouvette ep) {
+        theta.clear();
+        omega.clear();
         this.masse = uneMasse;
         this.longueur = uneLongueur;
         this.longueurReelle = longueur / 100.0;
@@ -48,7 +63,9 @@ public class Pendule {
         g.setColor(Color.red);
         g.fillOval(finX - 50, finY - 50, 100, 100);
         //tabX[0] = centreX - r*Math.cos(angle);
+        majEnergie();
         testCollision();
+        testLimite();
  
      }
 
@@ -57,16 +74,28 @@ public class Pendule {
      }
 
      public void testCollision() {
-        if(ep.estVivant && theta.getLast() < 0.01 && theta.getLast() > - 0.01 && energieCinetique < ep.unMateriau.Resilience) {
-            theta.clear();
-            omega.clear();
-            energieCinetique -= ep.unMateriau.Resilience;
-            theta.add(0.0);
+        if((ep.estVivant && theta.getLast() <= 0)  && (energieCinetique < ep.unMateriau.Resilience)) {
+            //theta.clear();
+            //omega.clear();
+            energieCinetique *= 0.2;
+            theta.add(0.001);
             omega.add(Math.sqrt(2 * energieCinetique / this.masse) / this.longueurReelle);
             
+        } else if(ep.estVivant && (energieCinetique > ep.unMateriau.Resilience) && (theta.getLast() <= 0)) {
+            energieCinetique-= ep.unMateriau.Resilience;
+            omega.add(- Math.sqrt(2 * energieCinetique / this.masse) / this.longueurReelle);
+            ep.estVivant = false;
         }
 
 
+     }
+
+     public void testLimite() {
+         if(theta.getLast() > 3.12 || theta.getLast() < -3.12) {
+            f.setChrono(false);
+            JOptionPane.showMessageDialog(f,"Limite de simulation atteinte", "Erreur simulation", 0);
+            f.majPendule.doClick();
+         }
      }
 
      public String toString() {
